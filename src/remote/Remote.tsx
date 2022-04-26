c03fa53a855be481be9ade83e61a763d76a0c80c
Asont <94078125+Asont@users.noreply.github.com>
2022-04-26 21:40:56 +0300

    refactoring

--- src/remote/Remote.tsx
+++ src/remote/Remote.tsx
@@ -4,7 +4,6 @@ import styleContainer from "../common/container.module.css"
 
 const Remote = () => {
     return (
-
         <div className={style.remoteBlock}>
             <div className={`${styleContainer.container} ${style.container}`}>
                 <h3 className={style.title}>Рассматриваю варианты удаленной работы</h3>
