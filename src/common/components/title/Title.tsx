232dd265fbb2bb25876b5ac20acce47b3bfff7de
Asont <94078125+Asont@users.noreply.github.com>
2022-05-11 12:26:59 +0300

    refactoring

--- src/common/components/title/Title.tsx
+++ src/common/components/title/Title.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import style from "./Title.module.css"
 
 type TitleType = {
-    text:string
+    text: string
 }
 
-const Title = (props:TitleType) => {
+const Title = (props: TitleType) => {
     return (
         <h3 className={style.title}>{props.text}</h3>
     );
