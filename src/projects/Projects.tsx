83af49fea4db384bb4cebb3ad3a714d2eacefd0c
Asont <94078125+Asont@users.noreply.github.com>
2022-05-12 20:03:44 +0300

    refactoring

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import style from "./Projects.module.css"
+import style from "./Projects.module.scss"
 import containerCommon from "../common/styles/container.module.css"
 import Project from "./project/Project";
 import Title from '../common/components/title/Title';
+import todolist from './../assets/image/img1.png'
+import social from './../assets/image/img2.png'
 
 const Projects = () => {
+
+    const todoListImg = {
+        backgroundImage: `url(${todolist})`,
+    };
+    const socialImg = {
+        backgroundImage: `url(${social})`,
+    };
+
     return (
         <div className={style.projectBlock}>
             <div className={`${containerCommon.container} ${style.container}`}>
                 <Title text={"Projects"}/>
                 <div className={style.projectsContainer}>
-                    <Project title={"Realistic Business Cards"} description={"Today we’re happy to share with you the mockup of a classic notebook that will help you showcase your drawings. Today we’re happy to share with you the mockup of a classic notebook that will help."}/>
-                    <Project title={"Juice Bottle Packaging"} description={"Today we have for you a set of 20 icons available in 2 styles: colored flat and line version, designed on a 128px grid. These polished icons come in multiple formats so you can easily include them in your projects."}/>
+                    <Project  style={todoListImg} title={"Realistic Business Cards"} description={"Today we’re happy to share with you the mockup of a classic notebook that will help you showcase your drawings. Today we’re happy to share with you the mockup of a classic notebook that will help."}/>
+                    <Project style={socialImg} title={"Juice Bottle Packaging"} description={"Today we have for you a set of 20 icons available in 2 styles: colored flat and line version, designed on a 128px grid. These polished icons come in multiple formats so you can easily include them in your projects."}/>
                     <Project title={"Mug PSD MockUp"} description={"Mapogo is an expertly crafted UI kit perfect for iOS designers and developers. The free sample includes 10 screens (750×1334 px) that can be easily customized in Photoshop"}/>
                     <Project title={"A4 Paper PSD"} description={"The freebie of the day is Chameleon, a modern UI kit perfect to use for creating a stylish and clean mobile app. This kit includes 10 screens from various categories that can be easily edited."}/>
                 </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import style from "./Project.module.css"
+import style from "./Project.module.scss"
 
 type ProjectType = {
     title: string,
     description: string
+    style?:{backgroundImage: string}
 }
 
 const Project = (props: ProjectType) => {
     return (
         <div className={style.projectBlock}>
-            <div className={style.imgContainer}>
+            <div className={style.imgContainer} style={props.style}>
                 <a href={""} className={style.link}>Посмотреть</a>
             </div>
             <div className={style.titleAndDesription}>
