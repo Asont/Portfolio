82ddb6525e51ac6157a2d6729c4d821466379fac
Asont <94078125+Asont@users.noreply.github.com>
2022-05-22 09:08:50 +0300

    refactoring

--- src/common/components/title/Title.tsx
+++ src/common/components/title/Title.tsx
@@ -4,7 +4,6 @@ import style from "./Title.module.scss"
 type TitleType = {
     text: string
 }
-
 const Title = (props: TitleType) => {
     return (
         <h3 className={style.title}>{props.text}</h3>
